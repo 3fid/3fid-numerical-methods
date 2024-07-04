6e02a9907d1913083792928db818683227121b03
3fid <bar.filip@email.cz>
2024-07-04 11:27:26 +0200

    txt

--- 4_romberg_integration.py
+++ 4_romberg_integration.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Automaticky preklad z matlabu 
+# asi stoji dost zahovno
 
 def lichobeznik(funkce, odkud, kam, krok):
         xArr = np.arange(odkud, kam + krok, krok)
